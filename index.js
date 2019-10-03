d3f46c55d27ad0f47a679a1f5ef35079032c0993
William Aperance <aperance@gmail.com>
2019-10-03 01:27:29 -0700

    Wrapped game logic in generator

--- index.js
+++ index.js
@@ -1,63 +1,83 @@
-const size = 10;
+function* gameGenerator(size, startingUniverse) {
+  let currentUniverse = startingUniverse;
+  while (true) {
+    yield currentUniverse;
 
-const universe = [
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-].flat();
-console.table(reshape(universe));
+    let newUniverse = [];
+    for (let row = 0; row < size; row++) {
+      const rowPrev = row === 0 ? size - 1 : row - 1;
+      const rowNext = row === size - 1 ? 0 : row + 1;
 
-function step(universe) {
-  let newUniverse = [];
+      for (let col = 0; col < size; col++) {
+        const colPrev = col === 0 ? size - 1 : col - 1;
+        const colNext = col === size - 1 ? 0 : col + 1;
 
-  for (let row = 0; row < size; row++) {
-    const rowPrev = row === 0 ? size - 1 : row - 1;
-    const rowNext = row === size - 1 ? 0 : row + 1;
+        const neighborCount =
+          currentUniverse[size * rowPrev + colPrev] +
+          currentUniverse[size * rowPrev + col] +
+          currentUniverse[size * rowPrev + colNext] +
+          currentUniverse[size * row + colPrev] +
+          currentUniverse[size * row + colNext] +
+          currentUniverse[size * rowNext + colPrev] +
+          currentUniverse[size * rowNext + col] +
+          currentUniverse[size * rowNext + colNext];
 
-    for (let col = 0; col < size; col++) {
-      const colPrev = col === 0 ? size - 1 : col - 1;
-      const colNext = col === size - 1 ? 0 : col + 1;
+        const index = row * size + col;
 
-      const neighborCount =
-        universe[size * rowPrev + colPrev] +
-        universe[size * rowPrev + col] +
-        universe[size * rowPrev + colNext] +
-        universe[size * row + colPrev] +
-        universe[size * row + colNext] +
-        universe[size * rowNext + colPrev] +
-        universe[size * rowNext + col] +
-        universe[size * rowNext + colNext];
-
-      const index = row * size + col;
-
-      switch (neighborCount) {
-        case 2:
-          newUniverse[index] = universe[index];
-          break;
-        case 3:
-          newUniverse[index] = 1;
-          break;
-        default:
-          newUniverse[index] = 0;
-          break;
+        switch (neighborCount) {
+          case 2:
+            newUniverse[index] = currentUniverse[index];
+            break;
+          case 3:
+            newUniverse[index] = 1;
+            break;
+          default:
+            newUniverse[index] = 0;
+            break;
+        }
       }
     }
+    currentUniverse = newUniverse;
   }
-  console.table(reshape(newUniverse));
 }
 
-function reshape(arr) {
+function reshape(arr, size) {
   const oldArr = [...arr];
   const newArr = [];
-  while (oldArr.length) newArr.push(oldArr.splice(0, 10));
+  while (oldArr.length) newArr.push(oldArr.splice(0, size));
   return newArr;
 }
 
-step(universe);
+const startingUniverse = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+const game = gameGenerator(25, startingUniverse.flat());
+
+setInterval(() => {
+  console.table(reshape(game.next().value, 25));
+}, 500);
