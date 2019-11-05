32144db9296fdeed4af7e18b5cc84d4a21f5d6a7
William Aperance <aperance@gmail.com>
2019-11-04 23:09:51 -0800

    Removed need for universe copy by deferring universe mutations until end of checking

--- gameEngine.js
+++ gameEngine.js
@@ -1,64 +1,93 @@
 function* gameEngine(size, initialAlive) {
   let universe = new Uint8Array(size * size);
-  let pastUniverse, born, died, alive;
+  let born, died, alive;
   let generation = 0;
   let cellsToCheck;
   let checkNextTime = new Set();
 
-  for (let i = 0, n = initialAlive.length; i < n; i++) {
-    const index = initialAlive[i];
-    universe[index] = 1;
-    const neighbors = getNeighbors(index, size);
-    checkNextTime.add(index);
+  for (let cellIndex of initialAlive) {
+    universe[cellIndex] = 1;
+    const neighbors = getNeighbors(cellIndex, size);
+    checkNextTime.add(cellIndex);
     for (let n of neighbors) checkNextTime.add(n);
   }
 
   while (true) {
     cellsToCheck = checkNextTime.values();
     checkNextTime = new Set();
-    pastUniverse = new Uint8Array(universe);
+
     born = [];
     died = [];
     alive = [];
+    generation++;
 
     for (const cellIndex of cellsToCheck) {
+      const prevState = universe[cellIndex];
       const neighbors = getNeighbors(cellIndex, size);
       let aliveNeighborCount = 0;
+      let isAlive = false;
+      let isChanged = false;
 
-      for (let i = 0; i < 8; i++) {
-        aliveNeighborCount += pastUniverse[neighbors[i]];
-      }
+      for (let neighborIndex of neighbors)
+        aliveNeighborCount += universe[neighborIndex];
 
       switch (aliveNeighborCount) {
         case 2:
-          universe[cellIndex] = pastUniverse[cellIndex];
+          isAlive = prevState === 1 ? true : false;
           break;
         case 3:
-          universe[cellIndex] = 1;
-          if (pastUniverse[cellIndex] === 0) born.push(cellIndex);
+          isAlive = true;
+          isChanged = prevState === 0 ? true : false;
           break;
         default:
-          universe[cellIndex] = 0;
-          if (pastUniverse[cellIndex] === 1) died.push(cellIndex);
+          isAlive = false;
+          isChanged = prevState === 1 ? true : false;
           break;
       }
 
-      if (universe[cellIndex] === 1) {
+      if (isAlive) {
         alive.push(cellIndex);
         checkNextTime.add(cellIndex);
       }
 
-      if (universe[cellIndex] !== pastUniverse[cellIndex]) {
+      if (isChanged) {
+        if (isAlive) born.push(cellIndex);
+        else died.push(cellIndex);
         checkNextTime.add(cellIndex);
         for (let n of neighbors) checkNextTime.add(n);
       }
     }
 
-    generation++;
+    for (let cellIndex of born) universe[cellIndex] = 1;
+    for (let cellIndex of died) universe[cellIndex] = 0;
 
     yield { born, died, alive, generation };
   }
 }
+
+// function getNeighbors(index, size, wrap) {
+//   const row = Math.floor(index / size);
+//   const col = index % size;
+
+//   let arr = [];
+
+//   if (row !== 0) {
+//     if (col !== 0) arr.push(index - size - 1);
+//     arr.push(index - size);
+//     if (col !== size - 1) arr.push(index - size + 1);
+//   }
+
+//   if (col !== 0) arr.push(index - 1);
+//   if (col !== size - 1) arr.push(index + 1);
+
+//   if (row !== size - 1) {
+//     if (col !== 0) arr.push(index + size - 1);
+//     arr.push(index + size);
+//     if (col !== size - 1) arr.push(index + size + 1);
+//   }
+
+//   return arr;
+// }
 
 function getNeighbors(index, size) {
   const row = Math.floor(index / size);
