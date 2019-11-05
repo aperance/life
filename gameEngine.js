c55fb176ef0d3c1fe27a7406cf1ceee418362e73
William Aperance <aperance@gmail.com>
2019-11-05 01:58:46 -0800

    Added no-wrap option to getNeighbors function

--- gameEngine.js
+++ gameEngine.js
@@ -65,48 +65,29 @@ function* gameEngine(size, initialAlive) {
   }
 }
 
-// function getNeighbors(index, size, wrap) {
-//   const row = Math.floor(index / size);
-//   const col = index % size;
-
-//   let arr = [];
-
-//   if (row !== 0) {
-//     if (col !== 0) arr.push(index - size - 1);
-//     arr.push(index - size);
-//     if (col !== size - 1) arr.push(index - size + 1);
-//   }
-
-//   if (col !== 0) arr.push(index - 1);
-//   if (col !== size - 1) arr.push(index + 1);
-
-//   if (row !== size - 1) {
-//     if (col !== 0) arr.push(index + size - 1);
-//     arr.push(index + size);
-//     if (col !== size - 1) arr.push(index + size + 1);
-//   }
-
-//   return arr;
-// }
-
 function getNeighbors(index, size) {
+  const wrap = false;
   const row = Math.floor(index / size);
   const col = index % size;
-  const rowPrev = row === 0 ? size - 1 : row - 1;
-  const rowNext = row === size - 1 ? 0 : row + 1;
-  const colPrev = col === 0 ? size - 1 : col - 1;
-  const colNext = col === size - 1 ? 0 : col + 1;
+  const max = size - 1;
 
-  return [
-    size * rowPrev + colPrev,
-    size * rowPrev + col,
-    size * rowPrev + colNext,
-    size * row + colPrev,
-    size * row + colNext,
-    size * rowNext + colPrev,
-    size * rowNext + col,
-    size * rowNext + colNext
-  ];
+  const rowBack = row === 0 ? size * max : -size;
+  const rowFwd = row === max ? -size * max : size;
+  const colBack = col === 0 ? max : -1;
+  const colFwd = col === max ? -max : 1;
+
+  let arr = [];
+
+  if (wrap || (row !== 0 && col !== 0)) arr.push(index + rowBack + colBack);
+  if (wrap || row !== 0) arr.push(index + rowBack);
+  if (wrap || (row !== 0 && col !== max)) arr.push(index + rowBack + colFwd);
+  if (wrap || col !== 0) arr.push(index + colBack);
+  if (wrap || col !== max) arr.push(index + colFwd);
+  if (wrap || (row !== max && col !== 0)) arr.push(index + rowFwd + colBack);
+  if (wrap || row !== max) arr.push(index + rowFwd);
+  if (wrap || (row !== max && col !== max)) arr.push(index + rowFwd + colFwd);
+
+  return arr;
 }
 
 export { gameEngine };
