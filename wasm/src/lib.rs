5b8a55d519cc9b5018312fc51d988923c75ae8f9
William Aperance <aperance@gmail.com>
2019-11-05 23:59:13 -0800

    Refactored wasm logic to match js implementation

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,6 +3,7 @@ extern crate wasm_bindgen;
 mod utils;
 
 use serde::Serialize;
+use std::collections::HashSet;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -15,6 +16,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub struct GameEngine {
     size: usize,
     data: Vec<u8>,
+    to_check: Vec<usize>,
 }
 
 #[wasm_bindgen]
@@ -32,10 +34,16 @@ impl GameEngine {
         let mut game_engine = GameEngine {
             size: size,
             data: Vec::new(),
+            to_check: Vec::new(),
         };
         game_engine.data.resize(size * size, 0);
 
         for cell_index in startingData {
+            let neighbors = get_neighbors(size, cell_index);
+            for neighbor_index in &neighbors {
+                game_engine.to_check.push(*neighbor_index);
+            }
+            game_engine.to_check.push(cell_index);
             game_engine.data[cell_index] = 1;
         }
 
@@ -61,42 +69,63 @@ impl GameEngine {
     }
 
     fn next(&mut self) -> Result {
-        let prev_data = self.data.clone();
-        let mut born = Vec::new();
-        let mut died = Vec::new();
-        let mut alive = Vec::new();
+        let mut to_check_next = HashSet::new();
+        let mut born: Vec<usize> = Vec::new();
+        let mut died: Vec<usize> = Vec::new();
+        let mut alive: Vec<usize> = Vec::new();
 
-        for row in 0..self.size {
-            for col in 0..self.size {
-                let index = self.size * row + col;
-                let neighbors = self.get_neighbors(index);
-                let mut alive_neighbor_count = 0;
+        for index in self.to_check.drain(..) {
+            let prev_state = self.data[index];
+            let mut is_alive = false;
+            let mut is_changed = false;
+            let neighbors = get_neighbors(self.size, index);
+            let mut alive_neighbor_count = 0;
 
-                for neighbor_index in neighbors {
-                    alive_neighbor_count = alive_neighbor_count + prev_data[neighbor_index];
+            for neighbor_index in &neighbors {
+                alive_neighbor_count = alive_neighbor_count + self.data[*neighbor_index];
+            }
+
+            match alive_neighbor_count {
+                2 => is_alive = if prev_state == 1 { true } else { false },
+                3 => {
+                    is_alive = true;
+                    is_changed = if prev_state == 0 { true } else { false }
                 }
-
-                match alive_neighbor_count {
-                    2 => self.data[index] = prev_data[index],
-                    3 => {
-                        self.data[index] = 1;
-                        if prev_data[index] == 0 {
-                            born.push(index)
-                        }
-                    }
-                    _ => {
-                        self.data[index] = 0;
-                        if prev_data[index] == 1 {
-                            died.push(index)
-                        }
-                    }
+                _ => {
+                    is_alive = false;
+                    is_changed = if prev_state == 1 { true } else { false }
                 }
+            }
 
-                if self.data[index] == 1 {
-                    alive.push(index)
-                };
+            if is_alive {
+                alive.push(index);
+                to_check_next.insert(index);
+            }
+
+            if is_changed {
+                if is_alive {
+                    born.push(index)
+                } else {
+                    died.push(index)
+                }
+                to_check_next.insert(index);
+                for neighbor_index in &neighbors {
+                    to_check_next.insert(*neighbor_index);
+                }
             }
         }
+
+        for cell_index in &born {
+            self.data[*cell_index] = 1
+        }
+        for cell_index in &died {
+            self.data[*cell_index] = 0
+        }
+
+        for cell_index in to_check_next.drain() {
+            self.to_check.push(cell_index)
+        }
+
         Result {
             born: born,
             died: died,
@@ -163,4 +192,45 @@ impl Result {
     pub fn alive(&self) -> Vec<usize> {
         self.alive.clone()
     }
+}
+
+fn get_neighbors(size: usize, index: usize) -> Vec<usize> {
+    let wrap = true;
+    let max = size - 1;
+    let row = index / size;
+    let col = index % size;
+
+    let row_prev = if row == 0 { max } else { row - 1 };
+    let row_next = if row == max { 0 } else { row + 1 };
+    let col_prev = if col == 0 { max } else { col - 1 };
+    let col_next = if col == max { 0 } else { col + 1 };
+
+    let mut arr = Vec::new();
+
+    if wrap || (row != 0 && col != 0) {
+        arr.push(size * row_prev + col_prev)
+    };
+    if wrap || row != 0 {
+        arr.push(size * row_prev + col)
+    };
+    if wrap || (row != 0 && col != max) {
+        arr.push(size * row_prev + col_next)
+    };
+    if wrap || col != 0 {
+        arr.push(size * row + col_prev)
+    };
+    if wrap || col != max {
+        arr.push(size * row + col_next)
+    };
+    if wrap || (row != max && col != 0) {
+        arr.push(size * row_next + col_prev)
+    };
+    if wrap || row != max {
+        arr.push(size * row_next + col)
+    };
+    if wrap || (row != max && col != max) {
+        arr.push(size * row_next + col_next)
+    };
+
+    arr
 }
