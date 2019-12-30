2b4e1f3233843f530f5a70320ffd93707669de9c
William Aperance <aperance@gmail.com>
2019-12-29 23:47:38 -0800

    Added update observer method

--- src/mouseTracker.js
+++ src/mouseTracker.js
@@ -5,9 +5,10 @@
  * @typedef {Object} MouseTracker
  * @property {boolean} down
  * @property {boolean} panning
- * @property {Array<Array<number>> | null} draggedShape
+ * @property {Array<Array<number>> | null} pattern
  * @property {number | null} lastX
  * @property {number | null} lastY
+ * @property {function(): void} updateObserver
  * @property {function(Array<Array<number>>): void} setPattern
  * @property {function(): void} clearPattern
  * @property {function(MouseEvent): void} canvasEnter
@@ -22,13 +23,13 @@
  *
  * @param {import('./gameRenderer').GameRenderer} gameRenderer
  * @param {import('./gameController').GameController} gameController
- * @param  {function(boolean, boolean): void} onChange
+ * @param  {function(boolean, boolean): void} observer
  * @returns {MouseTracker}
  */
-const createMouseTracker = (gameRenderer, gameController, onChange) => {
+const createMouseTracker = (gameRenderer, gameController, observer) => {
   /** @type {MouseTracker} */
   const mouseTracker = {
-    draggedShape: null,
+    pattern: null,
     down: false,
     panning: false,
     lastX: null,
@@ -39,16 +40,16 @@ const createMouseTracker = (gameRenderer, gameController, onChange) => {
      * @param {*} pattern
      */
     setPattern(pattern) {
-      this.draggedShape = pattern;
-      onChange(this.panning, this.draggedShape ? true : false);
+      this.pattern = pattern;
+      this.updateObserver();
     },
 
     /**
      *
      */
     clearPattern() {
-      this.draggedShape = null;
-      onChange(this.panning, this.draggedShape ? true : false);
+      this.pattern = null;
+      this.updateObserver();
     },
 
     /**
@@ -66,7 +67,7 @@ const createMouseTracker = (gameRenderer, gameController, onChange) => {
      *
      */
     canvasLeave() {
-      if (this.draggedShape !== null) gameController.clearPreview();
+      if (this.pattern !== null) gameController.clearPreview();
     },
 
     /**
@@ -75,21 +76,20 @@ const createMouseTracker = (gameRenderer, gameController, onChange) => {
      */
     canvasUp(e) {
       if (!this.panning) {
-        if (this.draggedShape === null)
+        if (this.pattern === null)
           gameController.toggleCell(e.clientX, e.clientY);
-        else
-          gameController.placeElement(e.clientX, e.clientY, this.draggedShape);
+        else gameController.placeElement(e.clientX, e.clientY, this.pattern);
       }
 
-      if (this.draggedShape !== null)
-        gameController.placePreview(e.clientX, e.clientY, this.draggedShape);
+      if (this.pattern !== null)
+        gameController.placePreview(e.clientX, e.clientY, this.pattern);
 
       this.panning = false;
       this.down = false;
       this.lastX = null;
       this.lastY = null;
 
-      onChange(this.panning, this.draggedShape ? true : false);
+      this.updateObserver();
     },
 
     /**
@@ -107,17 +107,17 @@ const createMouseTracker = (gameRenderer, gameController, onChange) => {
      * @param {MouseEvent} e
      */
     canvasMove(e) {
-      if (this.draggedShape !== null && !this.panning)
-        gameController.placePreview(e.clientX, e.clientY, this.draggedShape);
+      if (this.pattern !== null && !this.panning)
+        gameController.placePreview(e.clientX, e.clientY, this.pattern);
 
       if (this.down && this.lastX && this.lastY) {
         const deltaX = this.lastX - e.clientX;
         const deltaY = this.lastY - e.clientY;
 
         if (Math.abs(deltaX) > 2 || Math.abs(deltaY) > 2) {
-          if (this.draggedShape !== null) gameController.clearPreview();
+          if (this.pattern !== null) gameController.clearPreview();
           this.panning = true;
-          onChange(this.panning, this.draggedShape ? true : false);
+          this.updateObserver();
         }
 
         if (this.panning) {
@@ -140,6 +140,10 @@ const createMouseTracker = (gameRenderer, gameController, onChange) => {
       const { zoom } = gameRenderer.view;
       const newZoom = Math.round(zoom + Math.sign(e.deltaY) * (1 + zoom / 50));
       gameRenderer.zoomAtPoint(newZoom, e.clientX, e.clientY);
+    },
+
+    updateObserver() {
+      observer(this.panning, this.pattern ? true : false);
     }
   };
 
