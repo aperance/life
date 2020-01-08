7593e0a37cbe7d65a4173ae8fb340b85c10d62ae
William Aperance <aperance@gmail.com>
2020-01-07 21:52:15 -0800

    Added patterns, fixed bug where empty lines in rle were skipped

--- src/patterns.js
+++ src/patterns.js
@@ -1,5 +1,19 @@
 const categories = {
-  Guns: ["lwss", "mwss", "hwss"]
+  Spaceships: ["copperhead", "glider", "loafer", "lwss", "mwss", "hwss"],
+  Guns: ["bigun", "gosperglidergun", "p41660p5h2v0gun", "simkinglidergun"],
+  Oscillators: [
+    "beacon",
+    "blinker",
+    "blocker",
+    "figureeight",
+    "pentadecathlon",
+    "pulsar",
+    "queenbeeshuttle",
+    "tannersp46",
+    "toad",
+    "twinbeesshuttle"
+  ],
+  "Gardens of Eden": ["gardenofeden1", "gardenofeden4", "gardenofeden5"]
 };
 
 /**
@@ -36,7 +50,7 @@ export const patternLibrary = {
    */
   async loadFromFile(id) {
     const file = await import(`../patterns/${id}.rle`);
-    const data = file.default.split(/x.*S23/);
+    const data = file.default.split(/x.*[sS]23/);
     const rle = data[1].replace(/(\r\n|\n|\r)/gm, "");
 
     this.patternMap.set(id, {
@@ -96,111 +110,30 @@ export const patternLibrary = {
  *
  * @param {string} rle
  * @returns {number[][]}
+ * @throws
  */
-const rleToArray = rle =>
-  rle
-    .split("!")[0]
-    .split("$")
-    .map(substring => {
-      /** @type {number[]} */
-      let arr = [];
-      let temp = "";
-      substring.split("").forEach(char => {
-        if (char !== "b" && char !== "o") temp += char;
-        else {
-          const count = parseInt(temp, 10) || 1;
-          for (let i = 0; i < count; i++) {
-            if (char === "b") arr.push(0);
-            if (char === "o") arr.push(1);
-          }
-          temp = "";
+const rleToArray = rle => {
+  /** @type {number[][]} */
+  let outerArray = [];
+  /** @type {number[]} */
+  let innerArray = [];
+  let countString = "";
+
+  Array.from(rle).forEach(char => {
+    if (char >= "0" && char <= "9") countString += char;
+    else if (char === "b" || char === "o" || char === "$" || char === "!") {
+      const count = parseInt(countString, 10) || 1;
+      for (let i = 0; i < count; i++) {
+        if (char === "b") innerArray.push(0);
+        if (char === "o") innerArray.push(1);
+        if (char === "$" || char === "!") {
+          outerArray.push(innerArray);
+          innerArray = [];
         }
-      });
-      return arr;
-    });
+      }
+      countString = "";
+    } else throw new Error("Invalid character in pattern file");
+  });
 
-// const patternMap = new Map();
-
-// export const loadPatternFiles = () => {
-//   return Promise.all(
-//     Object.values(categories)
-//       .flat()
-//       .map(id => readFile(id))
-//   );
-// };
-
-// const readFile = async id => {
-//   const file = await import(`../patterns/${id}.rle`);
-//   const data = file.default.split(/x.*S23/);
-
-//   patternMap.set(id, {
-//     name: data[0].match(/(?<=#N ).*/)[0],
-//     author: data[0].match(/(?<=#O ).*/)[0],
-//     description: data[0].match(/(?<=#C ).*/g),
-//     rle: data[1].replace(/(\r\n|\n|\r)/gm, "")
-//   });
-// };
-
-// /**
-//  *
-//  * @param {string} id
-//  * @returns {{array: Array<Array<number>>}}
-//  */
-// export const getPatternData = id => {
-//   const patternData = patternMap.get(id);
-
-//   const array = patternData.rle
-//     .split("!")[0]
-//     .split("$")
-//     .map(substring => {
-//       let arr = [];
-//       let temp = "";
-//       substring.split("").forEach(char => {
-//         if (char !== "b" && char !== "o") temp += char;
-//         else {
-//           const count = parseInt(temp, 10) || 1;
-//           for (let i = 0; i < count; i++) {
-//             if (char === "b") arr.push(0);
-//             if (char === "o") arr.push(1);
-//           }
-//           temp = "";
-//         }
-//       });
-//       return arr;
-//     });
-
-//   return { array, ...patternData };
-// };
-
-// /**
-//  * @returns {string}
-//  */
-// export const generatePatternList = () => `
-//   <div class="list-group">
-//     ${Object.entries(categories)
-//       .map(
-//         ([category, contents], index) =>
-//           `<a class="list-group-item list-group-item-action collapse-link"
-//             data-toggle="collapse"
-//             href="#category${index}">
-//             ${category}
-//           </a>
-//           <div id="category${index}" class="collapse">
-//             ${contents
-//               .map(
-//                 id =>
-//                   `<a href="#"
-//                     class="pattern-name list-group-item list-group-item-action"
-//                     data-dismiss="modal"
-//                     data-pattern="${id}"
-//                   >
-//                     ${patternMap.get(id).name}
-//                   </a>`
-//               )
-//               .join("")}
-//           </div>
-//             `
-//       )
-//       .join("")}
-//   </div>
-// `;
+  return outerArray;
+};
